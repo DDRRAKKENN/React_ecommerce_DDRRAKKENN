e4d4f833b933bd8df7d639f67f9c1e5b0b7961a2
Jorge Ignacio Lara Ceballos <zero19b@hotmail.com>
2020-12-18 09:07:09 -0300

    base de datos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import { BrowserRouter} from 'react-router-dom' 
 import Menu from "./Components/Menu/Index"
 import RoutesWeb from "./Components/RoutesWeb/RoutesWeb"
+import firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAsfu7FdnBYLptHqJtb1dpL_ZCV2XlAtKk",
+    authDomain: "react-utn-lara.firebaseapp.com",
+    projectId: "react-utn-lara",
+    storageBucket: "react-utn-lara.appspot.com",
+    messagingSenderId: "385256768407",
+    appId: "1:385256768407:web:c6d707edb033be26007ad2"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 function App(){
+    console.log(firebase.database())
     const opciones=[
         {
             path:"/login",
--- src/Components/Productos.js
+++ src/Components/Productos.js
@@ -2,15 +2,16 @@ import { Link } from 'react-router-dom'
 import Button from "react-bootstrap/Button"
 import Card from 'react-bootstrap/Card'
 
+
 function Productos(props){
     return(
         <div>
-            <Card style={{ width: '18rem' }}>
+            <Card style={{ width: '15rem' }} border="primary" bg="light">
                 <Card.Body>
                     <Card.Title><b>{props.productos.name}</b></Card.Title>
                     <Link to={"/productos/"+props.productos.id}><Button variant ="outline-primary">Ver Detalle</Button></Link>
                 </Card.Body>
-                </Card>
+            </Card>
         </div>
     );
 }
