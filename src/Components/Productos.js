37ba9c960141b66f09af8d0bfc6078a8b43d05b8
Jorge Ignacio Lara Ceballos <zero19b@hotmail.com>
2021-01-01 14:48:30 -0300

    recordar data() usandolo en doc desde firebase

--- src/Components/Productos.js
+++ src/Components/Productos.js
@@ -2,17 +2,14 @@ import { Link } from 'react-router-dom'
 import Button from "react-bootstrap/Button"
 import Card from 'react-bootstrap/Card'
 
-
 function Productos(props){
     return(
-        <div>
-            <Card style={{ width: '15rem' }} border="primary" bg="light">
-                <Card.Body>
-                    <Card.Title><b>{props.productos.data().name}</b></Card.Title>
-                    <Link to={"productos/"+props.productos.id}><Button variant ="outline-primary">Ver Detalle</Button></Link>
-                </Card.Body>
-            </Card>
-        </div>
+        <Card style={{ width: '14rem' }} border="primary" bg="light">
+            <Card.Body>
+                <Card.Title><b>{props.productos.data().name}</b></Card.Title>
+                <Link to={"/productos/"+props.productos.id}><Button variant ="outline-primary">Ver Detalle</Button></Link>
+            </Card.Body>
+        </Card>
     );
 }
 export default Productos; 
--- src/Pages/ProductoDetalle.js
+++ src/Pages/ProductoDetalle.js
@@ -1,28 +1,30 @@
 import React, { useEffect,useState } from 'react';
 import { Link } from 'react-router-dom';
-//import { getProducto } from '../Services/ProductosServices'
+import { getProducto } from '../Services/ProductosServices'
 import Button from "react-bootstrap/Button"
-import firebase from "../Config/firebase";
+import firebase from "../Config/firebase" 
 
 function ProductoDetalle(props){
-    const [producto ,setProducto]=useState({})
+    
+    const [producto,setProducto]=useState({})
+    
     useEffect(
         ()=>{
             firebase.db.doc("productos/"+props.match.params.id)
             .get()
-            .then(doc=>{
-                setProducto(doc)
+            .then((doc)=>{
+                setProducto(doc.data())
             })
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        }, []
+        },
     )
-    return(
+
+    return (
             <>
-                <h2>{producto.data().name}</h2>
-                <div><b>Nombre del producto: </b>{producto.data().name}.</div>
-                <div><b>Descripcion: </b>{producto.data().description}.</div>
-                <div><b>Precio al consumidor: </b>${producto.data().price}(Dolares Americanos).</div>
-                <Link><Button variant ="primary">Comprar!</Button></Link>
+            <h2>{producto.name}</h2>
+            <div><b>Nombre del producto: </b>{producto.name}.</div>
+            <div><b>Descripcion: </b>{producto.description}.</div>
+            <div><b>Precio al consumidor: </b>${producto.price} (Dolares Americanos).</div>
+            <Link><Button variant ="primary">Comprar!</Button></Link>
             </>
     )
 }
